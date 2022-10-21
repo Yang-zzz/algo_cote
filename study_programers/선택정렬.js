d4bdf1b6162eb6604ce1c6426b091b73a749eef1
Yangjisung <ygji6406@gmail.com>
2022-10-21 23:09:57 +0900

    Select_sort,bubble_sort

---
+++ study_programers/선택정렬.js
@@ -0,0 +1,20 @@
+// 선택 정렬
+// 배열에서 가장 작은 값 (or 큰 값)을 선택해 정해진 위치(인덱스0)로 보내는 정렬
+// part1. 배열에서 최솟값을 찾기
+// part2. 최솟값을 기준으로 배열요소들과 비교 후 위치 바꾸기
+
+const array = [5, 13, 3, 20, 9, 16, 1, 4];
+
+function select(arr) {
+    let min = 0;
+    for(let i=0; i<arr.length; i++) {
+        for(let j=i+1; j<arr.length; j++) {
+            if(arr[i] > arr[j]) {
+                min = arr[j];
+                // 배열에서 최솟값을 찾음.
+            }
+        }
+    }  
+}
+
+select(arr);
