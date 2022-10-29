3b016ce1c7f321b27be5ee1e32617b6d15bb2b08
Yangjisung <ygji6406@gmail.com>
2022-10-30 00:29:45 +0900

    10/30_pare_cote

--- 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
@@ -3,10 +3,26 @@ function solution(n) {
     let gggom = 1;
 // 팩토리얼 세고 싶은 아이 -> 0! -> 1! -> 2! -> 3! -> ... -> 10! -> .... -> 99!
     while(n >= checkNum) { // #1  - checkNum >= n
-        checkNum *= gggom;   // #2
+        checkNum *= gggom;   // #2 1 
         gggom += 1;          // #3
         console.log(`checkNum: ${checkNum}`)
         console.log(`gggom: ${gggom}`)
     }
     return gggom-2
+}
+
+
+
+function solution(n) {
+    var answer = 0;
+    let num = n;
+    for(let i=1; i<=10; i++) {
+        num /= i;
+        if(num === 1) {
+           return answer = i;
+        } else if (num < 1) {
+           return answer = i-1;
+        }
+    }
+    return answer;
 }
