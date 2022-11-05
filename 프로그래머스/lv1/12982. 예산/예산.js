dfb6e5f53f8cca00bb93faadb2488eac2df5d482
Yang_z <89332492+Yang-zzz@users.noreply.github.com>
2022-11-05 16:46:30 +0900

    [level 1] Title: 예산, Time: 3.70 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -0,0 +1,14 @@
+// d 배열을 순회하여 각 요소를 하나씩 뽑고, budget에서 요소의 숫자만큼 뺀다.
+// budget의 수가 0미만 까지만 뺄 수 있도록 하고,
+// 계산되지 않은 남은 요소의 갯수를 카운트하여 리턴한다.
+function solution(d, budget) {
+    let count = 0;
+    d.sort((a,b) => a-b);
+    console.log(d);
+    for(let i=0; i<d.length; i++) {
+        if(budget >= d[i]) {
+            budget -= d[i];
+            count += 1;
+        }
+    } return count;
+}
