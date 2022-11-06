870ad80acaf0e40f09a03ccc81bc2125698a2fcd
Yang_z <89332492+Yang-zzz@users.noreply.github.com>
2022-11-06 21:48:31 +0900

    [level 0] Title: 배열의 유사도, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,10 +1,15 @@
 function solution(s1, s2) {
-    let empty = 0;
-    for(let i=0; i<s1.length; i++) {
-        for(let j=0; j<s2.length; j++) {
-            if(s1[i] === s2[j]) {
-                empty += 1;
-            }
-        }
-    } return empty
+    const i = s1.filter((x) => s2.includes(x));
+    return i.length;
 }
+
+// function solution(s1, s2) {
+//     let empty = 0;
+//     for(let i=0; i<s1.length; i++) {
+//         for(let j=0; j<s2.length; j++) {
+//             if(s1[i] === s2[j]) {
+//                 empty += 1;
+//             }
+//         }
+//     } return empty
+// }
