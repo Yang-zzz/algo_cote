b5981a3e3d79af356c56473002f7e8ef7b9a2e17
Yang_z <89332492+Yang-zzz@users.noreply.github.com>
2022-11-06 22:01:57 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -0,0 +1,9 @@
+function solution(my_string, letter) {
+    const answer = my_string.split(letter).join("");
+    return answer;
+}
+
+// function solution(my_string, letter) {
+//     let reg = new RegExp(letter, "g");
+//     return my_string.replace(letter, "");
+// }
